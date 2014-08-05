4fc1071eb809f9a0843c6130729b1d29bf29fbc1
claudia-romano <claudia.romano06@gmail.com>
2014-08-05 12:26:31 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Points Of Interest
+=========
+
+A resource to highlight the points of interest of your products. Just a click to open a brief description of each point, allowing your user to get a deep and fast understanding of your product features.
+
+[Article on Cody](http://codyhouse.co/gem/points-of-interest/)
+
+[Demo](http://codyhouse.co/demo/points-of-interest/index.html)
+ 
+[Terms](http://codyhouse.co/terms/)
---
+++ js/main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+	//open interest point description
+	$('.cd-single-point').children('a').on('click', function(){
+		var selectedPoint = $(this).parent('li');
+		if( selectedPoint.hasClass('is-open') ) {
+			selectedPoint.removeClass('is-open').addClass('visited');
+		} else {
+			selectedPoint.addClass('is-open').siblings('.cd-single-point').removeClass('is-open');
+		}
+	});
+	//close interest point description
+	$('.cd-close-info').on('click', function(event){
+		event.preventDefault();
+		$(this).parents('.cd-single-point').eq(0).removeClass('is-open').addClass('visited');
+	});
+});
